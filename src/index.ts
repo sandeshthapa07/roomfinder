f8edc3cf5ebcb675bd5806abbcbdfa6147dc820f
Sandesh thapa <sandesh.thapa@braintip.ai>
2024-08-25 22:17:27 +0545

    cookie changes amde

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "https://sandeshthapa07.github.io/refreshfront/",
+    origin: "https://sandeshthapa07.github.io",
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization']
